f6bd56aa3ac18283a234dd0a119a93afa2152f14
Dmitriy <315overdose@gmail.com>
2023-08-29 16:21:15 +0300

    base style (difference SafeArea and View)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,28 +1,21 @@
-import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
 
 export const App = () => {
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <Text style={styles.dummyText}>Hello World!</Text>
-        <Button title="Tap me!" color="#841584" />
+    <View style={styles.container}>
+      <View>
+        <TextInput placeholder="Your course goal!" />
+        <Button title="Add goal!" color="#841584" />
       </View>
-    </SafeAreaView>
+      <View>
+        <Text>List of goal</Text>
+      </View>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100%',
-  },
-
-  dummyText: {
-    margin: 16,
-    padding: 16,
-    borderWidth: 2,
-    borderColor: 'red',
+    padding: 50,
   },
 })
