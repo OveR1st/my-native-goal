19166c4157bf74a91a21f6917058e13214e3b61a
Dmitriy <315overdose@gmail.com>
2023-09-14 18:32:23 +0300

    Ripple effect for android, opacity ios

--- src/shared/ui/goal_list/part/goal_item.tsx
+++ src/shared/ui/goal_list/part/goal_item.tsx
@@ -8,11 +8,15 @@ interface IGoalItemProps {
 
 const GoalItem: React.FC<IGoalItemProps> = ({ item, onDeleteItem }) => {
   return (
-    <Pressable onPress={() => onDeleteItem(item.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: '#2dd2df' }}
+        onPress={() => onDeleteItem(item.id)}
+        style={({ pressed }) => pressed && styles.goalPressed}
+      >
         <Text style={styles.goalText}>{item.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   )
 }
 
@@ -21,12 +25,16 @@ export default GoalItem
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: '#5e0acc',
   },
 
+  goalPressed: {
+    opacity: 0.5,
+  },
+
   goalText: {
+    padding: 8,
     color: 'white',
   },
 })
